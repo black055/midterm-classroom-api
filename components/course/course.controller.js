1760b93bc559a47d4c684aec822db9989af52273
T N <truong1320vv@gmail.com>
2021-11-17 17:51:26 +0700

    Adjusting get user id

--- components/course/course.controller.js
+++ components/course/course.controller.js
@@ -1,9 +1,10 @@
 import Course from "./course.model.js";
+import User from "../user/user.model.js";
 import randomstring from "randomstring";
 
 export default {
   getCourses: (req, res) => {
-    const userId = req.headers.userId;
+    const userId = req.user._id;
 
     //lay toan bo lop ma user co the la teacher hoac student
     Course.find({ $or: [{ author: userId }, { teachers: { $in: userId } }, { students: { $in: userId } }] })
@@ -19,23 +20,34 @@ export default {
 
   getOneCourse: (req, res) => {
     const _id = req.params.id; //id cua lop cu the
-    const userId = req.headers.userId;
+    const userId = req.user._id;
 
     //lay mot lop ma user co the la teacher hoac student
     Course.findById(_id)
       .or([{ teachers: { $in: userId } }, { students: { $in: userId } }])
       .lean()
-      .exec((e, c) => {
+      .exec(async (e, c) => {
         if (e) {
           return res.status(500).json({ message: e });
         }
+
+        //neu khong co ket qua
+        if (!(c && c._id)) {
+          return res.status(202).json({ message: "You're not in the class!" });
+        }
+
+        const students = await User.find({ _id: { $in: c.students } });
+        const teachers = await User.find({ _id: { $in: c.teachers } });
+
+        c.students = students.map((s) => s.firstname + " " + s.lastname);
+        c.teachers = teachers.map((s) => s.firstname + " " + s.lastname);
 
         return res.status(200).json({ payload: c });
       });
   },
 
   createCourse: (req, res) => {
-    const author = req.headers.userId;
+    const author = req.user._id;
     const { name, details, briefName } = req.body;
     const code = randomstring.generate(7);
 
@@ -60,10 +72,10 @@ export default {
   },
 
   userJoinCourse: (req, res) => {
-    const userId = req.headers.userId;
+    const userId = req.user._id;
     const code = req.query.code;
 
-    Course.findOneAndUpdate({ code }, { $push: { students: userId } }).exec((e, c) => {
+    Course.findOneAndUpdate({ code, students: { $ne: userId } }, { $push: { students: userId } }).exec((e, c) => {
       if (e) {
         return res.status(500).json({ message: e });
       }
