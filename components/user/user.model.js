f325e4f7418956684065862acdf82d51e8741cea
truongvukt2000 <truongvukt2000@gmail.com>
2021-11-19 16:27:47 +0700

    user

--- components/user/user.model.js
+++ components/user/user.model.js
@@ -1,22 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const User = new mongoose.Schema({
-    email: { 
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    firstname: String,
-    lastname: String,
-    courses: Array,
-    gender: {
-        type: String,
-        enum: ['Nam', 'Nữ', 'Khác']
-    },
+  studentID: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstname: String,
+  lastname: String,
+  courses: Array,
+  gender: {
+    type: String,
+    enum: ["Nam", "Nữ", "Khác"],
+  },
 });
 
 export default mongoose.model("user", User, "user");
